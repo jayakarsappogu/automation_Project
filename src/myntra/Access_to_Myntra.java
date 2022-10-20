365b06843e68e77ea8ceee04a719519b77be8bcb
Admin <Admin@Admin-PC>
2022-10-20 19:58:18 +0530

    commit done added chrome driver latest version

--- src/myntra/Access_to_Myntra.java
+++ src/myntra/Access_to_Myntra.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Access_to_Myntra {
 
 	public static void main(String[] args) throws InterruptedException  {
-     System.getProperty("webdriver.chrome.driver", "D:\\\\Selenium folder\\\\chromedriver.exe");
+     System.getProperty("webdriver.chrome.driver", "D:\\Selenium folder\\chromedriver.exe");
      ChromeDriver driver=new ChromeDriver();
      driver.get("https://www.amazon.in/");
      Thread.sleep(2000);
